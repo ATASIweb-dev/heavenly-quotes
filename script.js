2529834a1d82e3816be98e57b7e61baf72b1abc2
Atasi Chakraborty <atasi.c1324@gmail.com>
2026-05-13 08:58:25 +0530

    fixed cean image and background paths

--- script.js
+++ script.js
@@ -60,8 +60,8 @@ const ships = [
 
             "The world burns, yet I still reach for you."
         ],
-        image: "assets/characters/ce an pic.jpg",
-        background: "assets/backgrounds/ce an bg.jpg"
+        image: "assets/characters/Ce an pic.jpg",
+        background: "assets/backgrounds/Ce an bg.jpg"
     }
 
 ];
